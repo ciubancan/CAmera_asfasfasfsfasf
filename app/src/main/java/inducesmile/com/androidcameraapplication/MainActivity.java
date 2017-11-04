cb18a1f5d0f0249303640b0c63e20af96bbdb23f
ciubancan <31895533+ciubancan@users.noreply.github.com>
2017-11-04 17:13:21 +0200

    ddd

--- app/src/main/java/inducesmile/com/androidcameraapplication/MainActivity.java
+++ app/src/main/java/inducesmile/com/androidcameraapplication/MainActivity.java
@@ -1,10 +1,11 @@
 package inducesmile.com.androidcameraapplication;
 
 import android.content.Intent;
-import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
@@ -12,33 +13,68 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import org.opencv.core.Core;
+import android.widget.Toast;
 
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+
+import java.io.File;
+import java.util.UUID;
 
 import static inducesmile.com.androidcameraapplication.R.id.captured_photo;
 
 public class MainActivity extends ActionBarActivity {
 
-    static {
-        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-    }
-
-    private ImageView imageHolder;
-    private final int requestCode = 20;
-
+    private ImageView _imageHolder;
+    private String _pictureFilePath;
+    private final int TAKE_PICTURE_ACTIVITY = 20;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        imageHolder = (ImageView)findViewById(captured_photo);
+        BaseLoaderCallback loaderCallback = new BaseLoaderCallback(this) {
+            @Override
+            public void onManagerConnected(int status) {
+                super.onManagerConnected(status);
+                switch(status){
+                    case LoaderCallbackInterface.SUCCESS:
+                        Toast.makeText(getApplicationContext(), "manager connected", Toast.LENGTH_LONG).show();
+                        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+                        break;
+                    default:
+                        super.onManagerConnected(status);
+                        break;
+                }
+            }
+        };
+
+        if (!OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_13, this, loaderCallback))
+        {
+        }
+
+        _imageHolder = (ImageView)findViewById(captured_photo);
         Button capturedImageButton = (Button)findViewById(R.id.photo_button);
         capturedImageButton.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent photoCaptureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(photoCaptureIntent, requestCode);
+                UUID guid = UUID.randomUUID();
+                String pictureFileName = guid.toString() + ".jpg";
+                String destination =  Environment.getExternalStorageDirectory().getPath() + "/" + pictureFileName;
+                File tempFile = new File(destination);
+                Uri outputUri = Uri.fromFile(tempFile);
+                _pictureFilePath = tempFile.getAbsolutePath();
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, outputUri);
+                startActivityForResult(intent, TAKE_PICTURE_ACTIVITY);
             }
         });
     }
@@ -47,30 +83,25 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(this.requestCode == requestCode && resultCode == RESULT_OK){
-            ImageView image = (ImageView) findViewById(captured_photo);
-            imageHolder.setRotation(90.0f);
-            //Bitmap bMap = BitmapFactory.decodeFile("/sdcard1/DCIM/100ANDRO/DSC_0055.JPG");
-            //image.setImageBitmap(bMap);
-            //
-            final String[] imageColumns = { MediaStore.Images.Media._ID, MediaStore.Images.Media.DATA };
-            final String imageOrderBy = MediaStore.Images.Media._ID + " DESC";
-            Cursor imageCursor = managedQuery(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, imageColumns, null, null, imageOrderBy);
-            imageCursor.moveToFirst();
-            do {
-                String fullPath = imageCursor.getString(imageCursor.getColumnIndex(MediaStore.Images.Media.DATA));
-                if (fullPath.contains("DCIM")) {
-                    //--last image from camera --
-                    Bitmap bMap = BitmapFactory.decodeFile(fullPath);
-                    image.setImageBitmap(bMap);
-                    return;
-                }
-            }
-            while (imageCursor.moveToNext());
 
+        if(this.TAKE_PICTURE_ACTIVITY == requestCode && resultCode == RESULT_OK){
+            BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+            bmOptions.inPreferredConfig = Bitmap.Config.ARGB_8888;
 
-            //Bitmap bitmap = (Bitmap)data.getExtras().get("data");
-            //imageHolder.setImageBitmap(bitmap);
+            Bitmap myBitmap32 = BitmapFactory.decodeFile(_pictureFilePath);
+            Mat matImage = new Mat(myBitmap32.getHeight(), myBitmap32.getWidth(), CvType.CV_8UC3);
+            Utils.bitmapToMat(myBitmap32, matImage);
+            Mat gray = new Mat(matImage.size(), CvType.CV_8UC1);
+            Imgproc.cvtColor(matImage, gray, Imgproc.COLOR_RGB2GRAY, 4);
+            Mat edge = new Mat();
+            Mat dst = new Mat();
+            Imgproc.Canny(gray, edge, 60, 80);
+            Imgproc.cvtColor(edge, dst, Imgproc.COLOR_GRAY2RGBA, 4);
+            Bitmap resultBitmap = Bitmap.createBitmap(dst.cols(), dst.rows(), Bitmap.Config.ARGB_8888);
+            Utils.matToBitmap(dst, resultBitmap);
+
+            _imageHolder.setRotation(90);
+            _imageHolder.setImageBitmap(resultBitmap);
         }
     }
 
